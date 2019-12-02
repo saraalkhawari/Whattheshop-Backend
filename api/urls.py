cd2e80847ae90d8053c413c66f56b1d8bb001009
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-12-02 19:44:39 +0300

    uncommented url , update host

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 urlpatterns = [
 	path('', views.CreaturesList.as_view(), name="list"),
     path('login/', TokenObtainPairView.as_view() , name='login'),
-    # path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('register/', views.UserCreateAPIView.as_view(), name='register'),
 ]
